60d503352180cccd05ac9b01057f3ba15b48e33d
ramune314159265 <ramune314159265@gmail.com>
2024-08-24 18:09:41 +0900

    init

---
+++ src/main/java/ramune314159265/dateToXpBar/DateToXpBar.java
@@ -0,0 +1,17 @@
+package ramune314159265.dateToXpBar;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DateToXpBar extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
